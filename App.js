5dfa28c397d6e342167e717107fb0cda97104a98
zhangguohua <zhangguohua@yunintegration.com>
2018-02-22 09:58:44 +0800

    增加按钮和事件处理逻辑

--- App.js
+++ App.js
@@ -9,17 +9,24 @@ import {
   Platform,
   StyleSheet,
   Text,
+  Button,
+  Alert,
   View
 } from 'react-native';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu\n\n' +
+    '摇一摇或者按键Cmd+D出现开发人员菜单\n\n' +
 	'2018-02-22:开始学习React Native各种功能',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu\n\n' +
+  android: '连续两次按R键重新加载页面,\n' +
+    '摇一摇或者按菜单键弹出开发人员菜单\n\n' +
 	'2018-02-22:开始学习React Native各种功能',
 });
+
+const navigateToLoginScreen = () => {
+  Alert.alert('点击了 登录 按钮\n现在界面将要切换到 登录页面');
+  
+};
 
 type Props = {};
 export default class App extends Component<Props> {
@@ -35,6 +42,20 @@ export default class App extends Component<Props> {
         <Text style={styles.instructions}>
           {instructions}
         </Text>
+        <Text style={styles.instructions}>
+          如果你已有账号,请: 
+        </Text>			
+		<Button onPress={navigateToLoginScreen}
+			color = '#841584'
+			title="登录" accessibilityLabel="登录"
+		/>		
+        <Text style={styles.instructions}>
+          如果你还没有账号,请: 
+        </Text>		
+		<Button onPress={navigateToLoginScreen}
+			color = '#841584'
+			title="注册"
+		/>			
       </View>
     );
   }
@@ -44,8 +65,9 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center',
+    //alignItems: 'center', 
     backgroundColor: '#F5FCFF',
+	margin: 10,
   },
   welcome: {
     fontSize: 20,
--- README.md
+++ README.md
@@ -6,3 +6,11 @@
 react-native-cli: 2.0.1
 react-native: 0.53.3
 ```
+
+## 学习Button的使用
+
+1. 添加Button到页面
+2. Button在容器中的布局
+3. Button事件
+   1. 点击按钮时切换到另外一个页面:登录页面/注册页面
+   2. 问题 : Button 的 style 怎么设置 ? 宽度，在容器内的对齐等等 
