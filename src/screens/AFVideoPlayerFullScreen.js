2a0906160ecf43ced462541f394fc9ae807c8225
zhangguohua <zhangguohua@yunintegration.com>
2018-05-21 15:44:17 +0800

    ScrollView 中多个视频播放控件

--- src/screens/AFVideoPlayerFullScreen.js
+++ src/screens/AFVideoPlayerFullScreen.js
@@ -4,8 +4,6 @@ import Video from 'react-native-af-video-player';
 import toast from '../utils/ToastUtils'
 import Orientation from 'react-native-orientation';
 
-// 例子 : 使用网络视频地址
-//const url = "http://customer.test.qifuy.com/repo/upload/biz/government_regulation/2018/2/12/77046da6-7f9b-461c-87ed-44b60b641a15.mp4";
 
 const styles = StyleSheet.create({
     container: {
@@ -13,19 +11,15 @@ const styles = StyleSheet.create({
     }
 })
 
-class AFVideoPlayerTestScreen extends Component {
+class AFVideoPlayerFullScreen extends Component {
 
     static navigationOptions = ({navigation}) => {
         const {state} = navigation
-        // Setup the header and tabBarVisible status
-        const header = state.params && (state.params.fullscreen ? undefined : null)
-        const tabBarVisible = state.params ? state.params.fullscreen : true
+        const header = null
+
         return {
             // For stack navigators, you can hide the header bar like so
             header,
-            // For the tab navigators, you can hide the tab bar like so
-            tabBarVisible,
-            title: 'AF视频播放器测试',
         }
     }
 
@@ -91,14 +85,7 @@ class AFVideoPlayerTestScreen extends Component {
                 <ScrollView>
                     <Text>这是一个ScrollView中嵌入视频进行播放的例子</Text>
                 </ScrollView>
-                <ScrollView>
-                    <Text>动画大片:Big Buck Bunny</Text>
-                </ScrollView>
-                {this._renderVideo1()}
-                <ScrollView>
-                    <Text>Small Toy</Text>
-                </ScrollView>
-                {this._renderVideo2()}
+                {this._renderVideo1}
                 <ScrollView>
                     <Text>这个视频效果怎么样 ...... </Text>
                 </ScrollView>
@@ -107,37 +94,20 @@ class AFVideoPlayerTestScreen extends Component {
     }
 
     _renderVideo1() {
-        // 例子 : 本地视频文件
-        const url = require("../assets/videos/BigBuckBunny.mp4");
         const logo = 'http://img.bimg.126.net/photo/RxnoPmwl9wRK8fx8AokiGQ==/5753911473919624232.jpg'
         const placeholder = 'http://e.hiphotos.baidu.com/image/pic/item/d6ca7bcb0a46f21fca6fafecfa246b600c33ae32.jpg'
-        const title = 'Big Buck Bunny'
+        const title = '测试视频文件1'
         return (<Video
-            url={url}
-            autoPlay={false}
+            url={url1}
+            autoPlay
             title={title}
             logo={logo}
             placeholder={placeholder}
             onMorePress={() => this.onMorePress()}
             onFullScreen={status => this.onFullScreen(status)}
+
         />);
     }
-
-    _renderVideo2() {
-        // 例子 : 本地视频文件
-        const url = require("../assets/videos/small.mp4");
-        const placeholder = 'http://e.hiphotos.baidu.com/image/pic/item/d6ca7bcb0a46f21fca6fafecfa246b600c33ae32.jpg'
-        const title = '小玩具会转动'
-        return (<Video
-            url={url}
-            autoPlay={false}
-            title={title}
-            placeholder={placeholder}
-            onMorePress={() => this.onMorePress()}
-            onFullScreen={status => this.onFullScreen(status)}
-        />);
-    }
-
 }
 
-export default AFVideoPlayerTestScreen;
+export default AFVideoPlayerFullScreen;
